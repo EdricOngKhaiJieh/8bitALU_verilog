630cd77ab0bb2b442401931196b3d19419603c59
EdricOngKhaiJieh <119392734+EdricOngKhaiJieh@users.noreply.github.com>
2024-04-19 00:19:34 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -54,6 +54,11 @@ module tb ();
     reg IN5; 
     reg IN6; 
     reg IN7; 
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
  
     wire OUT0;
     wire OUT1;
@@ -83,7 +88,12 @@ tt_um_8bitALU user_project(
 .OUT4(OUT4),
 .OUT5(OUT5),
 .OUT6(OUT6),
-.OUT7(OUT7)
+.OUT7(OUT7),
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe)  // IOs: Enable path (active high: 0=input, 1=output)
 );
 endmodule
    
