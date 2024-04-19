1b70838b32f8689e5680d6509b3910de7d33d29c
EdricOngKhaiJieh <119392734+EdricOngKhaiJieh@users.noreply.github.com>
2024-04-19 00:16:46 -0700

    Update ALU_test.v

--- src/ALU_test.v
+++ src/ALU_test.v
@@ -1,6 +1,10 @@
 module tt_um_8bitALU (
 
- 
+   input  wire [7:0] ui_in,    // Dedicated inputs
+   output wire [7:0] uo_out,   // Dedicated outputs
+   input  wire [7:0] uio_in,   // IOs: Input path
+   output wire [7:0] uio_out,  // IOs: Output path
+   output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
   input  IN0,
   input  IN1, 
   input  IN2, 
